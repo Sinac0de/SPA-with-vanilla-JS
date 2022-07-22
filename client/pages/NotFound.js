245abff01b84510fb492970f7f972f03c42a6872
Sina Moradian <sinacsharp@gmail.com>
2022-07-23 01:34:37 +0430

    Add back-end stuff

---
+++ client/pages/NotFound.js
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return `<h1>متاسفانه صفحه مورد نظر یافت نشد!</h1>
+            <br>
+            <a href = "/" style="text-decoration: none;"> به داشبورد میروید؟ <a>`;
+};
+
+export default NotFound;
