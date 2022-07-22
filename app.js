ed7bab9cdc5b708b1038736897877cc9ed9e3ced
Sina Moradian <sinacsharp@gmail.com>
2022-07-22 21:41:49 +0430

    Add navigation function and popstate event

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //1.what to show to user based on the route
 
+
+/*----------------
+     FUNCTIONS 
+ ----------------*/
 //show content based on the route that user is on
 function router(params) {
     const routes = [
         { path: "/", view: () => console.log("dashboard page") },
-        { path: "products", view: () => console.log("products page") },
-        { path: "contanct-us", view: () => console.log("contanct-us page") },
+        { path: "/products", view: () => console.log("products page") },
+        { path: "/contanct-us", view: () => console.log("contanct-us page") },
     ];
 
     const potentialRoutes = routes.map(item => {
@@ -16,7 +20,7 @@ function router(params) {
     });
 
     //find the isMatch == true object
-    const match = potentialRoutes.find((route) => route.isMatch);
+    let match = potentialRoutes.find((route) => route.isMatch == true);
     console.log(match.route.view());
 
     //if page not found (like when user puts something randomly in the url)
@@ -28,5 +32,27 @@ function router(params) {
     }
 }
 
+//change url to new address
+function navigateTo(url) {
+    history.pushState(null, null, url);
+    router();
+}
+
+
+/*--------------------- 
+     Event Listeners
+ ---------------------*/
+
 //check the route when the page is loaded
-document.addEventListener("DOMContentLoaded", () => router());
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", (e) => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();//prevent refreshing the page
+            navigateTo(e.target.href);
+        }
+    });
+    router();
+});
+
+//call router function everytime the history changes
+window.addEventListener("popstate", router);
